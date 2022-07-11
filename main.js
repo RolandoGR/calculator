829e8e8cc064c5c643ea5be65f14aeb66dc40c72
Varrium <rolandogarciaram@gmail.com>
2022-07-11 14:26:24 -0700

    Change aesthetics of calculator

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-function operate(n1, n2) {
-    
+function add (n1, n2) {
+    return n1 + n2
 }
+function subtract (n1, n2) {
+    return n1 - n2
+}
+function multiply (n1, n2) {
+    return n1 * n2
+}
+function divide (n1, n2) {
+    return n1 / n2
+}
+
+function operate() {
+    let n1 = parseInt(prompt ("enter first number"))
+    let operator = prompt ("enter operator")
+    let n2 = parseInt(prompt ("enter second number"))
+    if (operator === '+') {
+        return add(n1, n2)
+    } else if (operator === '-') {
+        return subtract(n1, n2)
+    } else if (operator === '*') {
+        return multiply(n1, n2)
+    } else if (operator === '/') {
+        return divide(n1, n2)
+    } else {
+        operator = prompt ("enter operator again")
+    }
+ 
+}
+
 
 let input = document.getElementById('#number')
 console.log(input)
